e7d278e49dc20bd5cb2baac33cefb01ddba45c23
ITM_Student_08 <s10843041@ems.edu.tw>
2022-07-21 15:11:01 +0800

    delete every sample

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,21 +8,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { TransactionComponent } from './components/transaction/transaction.component';
 import { ProductComponent } from './components/product/product.component';
 import { RouterModule } from '@angular/router';
-import { BodyComponent } from './Sidebarsample/body/body.component';
-import { CoupensComponent } from './Sidebarsample/coupens/coupens.component';
-import { DashboardComponent } from './Sidebarsample/dashboard/dashboard.component';
-import { MediaComponent } from './Sidebarsample/media/media.component';
-import { PagesComponent } from './Sidebarsample/pages/pages.component';
-import { ProductsComponent } from './Sidebarsample/products/products.component';
-import { SettingsComponent } from './Sidebarsample/settings/settings.component';
-import { SidenavComponent } from './Sidebarsample/sidenav/sidenav.component';
-import { StatisticsComponent } from './Sidebarsample/statistics/statistics.component';
 import {MatSelectModule} from '@angular/material/select';
 import {MatIconModule} from '@angular/material/icon';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormCreateRestockComponent } from './form-create-restock/form-create-restock.component';
+import { FormCreateRestockComponent } from './components/form/Restock/form-create-restock/form-create-restock.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
@@ -30,15 +21,6 @@ import { ReactiveFormsModule } from '@angular/forms';
     AppComponent,
     TransactionComponent,
     ProductComponent,
-    BodyComponent,
-    CoupensComponent,
-    DashboardComponent,
-    MediaComponent,
-    PagesComponent,
-    ProductsComponent,
-    SettingsComponent,
-    SidenavComponent,
-    StatisticsComponent,
     FormCreateRestockComponent
   ],
   imports: [
